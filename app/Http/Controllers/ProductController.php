1964e1d368417aa1acdbd7a73f37e7e0c39133f0
lokentran <thanhtungmarine@gmail.com>
2020-08-23 22:15:32 +0700

    Built index function  in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use App\Http\Services\ProductService;
 
 class ProductController extends Controller
 {
-    //
+    protected $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        $products = $this->productService->getAll();
+        return view('backend.product.list', compact('products','categories'));
+    }
 }
