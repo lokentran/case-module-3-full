50e6a6c59c1115ec7dd607a4fb56e9409118956a
lokentran <thanhtungmarine@gmail.com>
2020-08-23 22:12:53 +0700

    Built ProductService (show index)

---
+++ app/Http/Services/ProductService.php
@@ -0,0 +1,26 @@
+<?php 
+namespace App\Http\Services;
+
+use App\Http\Repositories\ProductRepo;
+use App\Product;
+
+class ProductService {
+    protected $productRepo;
+
+    public function __construct(ProductRepo $productRepo)
+    {
+        $this->productRepo = $productRepo;
+    }
+
+    public function getAll() {
+        return $this->productRepo->getAll();
+    }
+
+    public function getById($id) {
+        return $this->productRepo->getById($id);
+    }
+
+
+
+}
+
